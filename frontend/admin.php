2fea348e20d9bd787b543ee8f3b9b8e270ffe003
andybd2703 <yeison.andres2006.yb@gmail.com>
2025-07-26 17:37:16 -0400

    logo a la pagina del administrador

--- frontend/admin.php
+++ frontend/admin.php
@@ -18,6 +18,7 @@ $resultado = $conexion->query($sql);
     <meta charset="UTF-8">
     <title>Reservas - Café La Loma</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="icon" type="image/jpeg" href="../img/logocf.jpg">
 </head>
 <body class="bg-light">
     <div class="container py-5">
